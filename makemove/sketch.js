daf594b048d1d982f28e5c12ffab11eb3d5e2812
Sujit <sjoshi1@saic.edu>
2020-02-25 06:19:15 -0600

    Update sketch.js

--- makemove/sketch.js
+++ makemove/sketch.js
@@ -1,8 +1,5 @@
 //spreadsheet link- https://docs.google.com/spreadsheets/d/e/2PACX-1vSNLla6JpobjcHF8gee2quZIBP4-CC4KYkyLx-WZ071DqKjBMeBhkTow9atPbgfaRkRurpQpTlrum-0/pubhtml
 // Device_name Device_ID	User_number	Event_1_count	Event_1_timestamp	Event_1_lon	Event_1_lat	Event_1_duration_start	Event_1_duration_end	Event_1_duration_lon	Event_1_duration_lat	Event_2_count	Event_2_timestamp	Event_2_lon	Event_2_lat	Event_2_duration_start	Event_2_duration_end	Event_2_duration_lon	Event_2_duration_lat		random_1	random_2	lon	lat	blanks
-let output;
-var x;
-var y;
 
 function setup (){
   createCanvas(400,400);
@@ -13,10 +10,13 @@ function setup (){
 function draw (){
 
   Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1qsBf8xvP98dHtfVLfY3CbJwmwojH_Z5wmUc0a0nyT2c/edit#gid=0',
-                    callback: function(data, tabletop) {
-                      //let data_20 = data[20].Event_2_timestamp;
-                      console.log(data)
-                      output=data;
+                    callback: function(Hakuna) {
+
                     },
                     simpleSheet: true } )
+}
+
+function Hakuna(data, tabletop) {
+ // do something with the data
+ console.log(JSON.stringify(data, null, 2));
 }
