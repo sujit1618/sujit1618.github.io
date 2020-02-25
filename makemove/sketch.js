451b4a8001694c10c54b5ae5e5f7662165486715
Sujit <sjoshi1@saic.edu>
2020-02-25 17:18:34 -0600

    Update sketch.js

--- makemove/sketch.js
+++ makemove/sketch.js
@@ -16,6 +16,6 @@ frameRate(0.15);
 
 function gotData(stuff,tabletop){
   data=stuff;
-  let totalCount = Num(data[1].Device_ID)+Num(data[2].Device_ID);
+  let totalCount = Number(data[1].Device_ID)+Number(data[2].Device_ID);
   console.log(totalCount);
 }
