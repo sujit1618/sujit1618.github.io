605c32b36dd907b06bf11416b4038733b2176f19
Sujit <sjoshi1@saic.edu>
2020-02-27 21:25:35 -0600

    Update sketch.js

--- makemove/sketch.js
+++ makemove/sketch.js
@@ -1,33 +1,51 @@
 //spreadsheet link- https://docs.google.com/spreadsheets/d/e/2PACX-1vSNLla6JpobjcHF8gee2quZIBP4-CC4KYkyLx-WZ071DqKjBMeBhkTow9atPbgfaRkRurpQpTlrum-0/pubhtml
 // Device_name Device_ID	User_number	Event_1_count	Event_1_timestamp	Event_1_lon	Event_1_lat	Event_1_duration_start	Event_1_duration_end	Event_1_duration_lon	Event_1_duration_lat	Event_2_count	Event_2_timestamp	Event_2_lon	Event_2_lat	Event_2_duration_start	Event_2_duration_end	Event_2_duration_lon	Event_2_duration_lat		random_1	random_2	lon	lat	blanks
+
+//chicago center
+let fps = 1/10;
+
+let baseLat= 41.8748065; //Y
+let baseLon= -87.625219;//X
+let lonRemap=0.1*baseLon/baseLat;
+let canvasX=600;
+let canvasY=400;
 var data;
+var posX=new Array(200);
+var posY=new Array(200);
+var magEvent1=new Array(200);
+
 function setup (){
-createCanvas(400, 400);
-frameRate(1);
+createCanvas(canvasX, canvasY);
+frameRate(fps);
+baseXfactor=canvasY/baseLon;
+baseYfactor=canvasX/baseLat;
 }
 
-function draw (){
+
+function draw(){
+var x = [1,2,3,4];
+console.log(x[1]);
+console.log(x[3]);
+downloadSheet();
+}
+
+
+function downloadSheet (){
 
   Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1qsBf8xvP98dHtfVLfY3CbJwmwojH_Z5wmUc0a0nyT2c/edit#gid=0',
                    callback: gotData,
                     simpleSheet: true
                   } );
-
-  frameRate(2);
 }
 
-function gotData(stuff,tabletop){
+// Device_name Device_ID	User_number	Event_1_count	Event_1_timestamp	Event_1_lon	Event_1_lat	Event_1_duration_start	Event_1_duration_end	Event_1_duration_lon	Event_1_duration_lat	Event_2_count	Event_2_timestamp	Event_2_lon	Event_2_lat	Event_2_duration_start	Event_2_duration_end	Event_2_duration_lon	Event_2_duration_lat		random_1	random_2	lon	lat	blanks
+
+function gotData(stuff,tabletop){ //function which works inside update loop
   data=stuff;
-  let x = data[1].Event_1_lat+data[1].Event_2_lat;
-  console.log(x);
-  data[1].Event_1_lat=Number(data[1].Event_1_lat);
-  data[1].Event_2_lat=Number(data[1].Event_2_lat);
-  let y = data[1].Event_1_lat+data[1].Event_2_lat;
-  console.log(y);
-  background(0);
-  ellipse(200,200,Number(data[1].Event_1_count),Number(data[1].Event_2_count));
-  fill(255,179,87);
-  triangle(200,200,Number(data[1].Event_1_count));
-  fill(255);
+
+  for(let i=0;i<200;i++){
+    data[i].Device_ID=Number(data[i].Device_ID);
+  }
+
 
 }
