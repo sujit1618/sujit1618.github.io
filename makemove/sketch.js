fc72a5df90023435cf8711037398c15d1090e7d9
Sujit <sjoshi1@saic.edu>
2020-03-03 14:54:32 -0600

    Update sketch.js

--- makemove/sketch.js
+++ makemove/sketch.js
@@ -195,7 +195,7 @@ function circlesEvents(i,j) {
       ellipse(canvasX/2 + 46 * f, 35.5 * d, factor * random(2 + event2Count(gh), event2Count(gh) - 2), factor * random(2 + event2Count(gh), event2Count(gh) - 2));
       noStroke();
       fill(white);
-      //text(event1Count(gh), halfCanvasX-46*f - 4.855, 35.5 * d-4.855); text(event2Count(gh), halfCanvasX + 46 * f - 4.855, d + 34.5 * d);
+      text(event1Count(gh), halfCanvasX-46*f - 4.855, 35.5 * d-4.855); text(event2Count(gh), halfCanvasX + 46 * f - 4.855, d + 34.5 * d);
       gh++;
     }
   }
