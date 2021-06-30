c002bea9e40dcab346886bbfc88521d1268815fe
Sujit <sjoshi1@saic.edu>
2021-06-30 10:22:19 +0530

    lat long for humanqind

--- codingwork/template-signup-form-master/main.js
+++ codingwork/template-signup-form-master/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+	  
   $('#signup').submit(function(e) {
+	  
 	e.preventDefault();
 	let form = $(this);
 	$.ajax({
---
+++ codingwork/template-signup-form-master/test1_script.js
@@ -0,0 +1,65 @@
+function getLocation(){
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log('Latitutde: ',latitude);
+    console.log('Longitude',longitude);
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+
+    postData(latitude,longitude);
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+function postData(lat,long) {
+    console.log(lat,' ',long);
+    let url = 'https://api.sheety.co/b5451f2c22c91b7055c9c9714d070461/test1Sheets/sheet1';
+    let body = {
+        sheet1: {
+            text: 'falalala more',
+            latValue: lat,
+            longValue: long
+        }
+    };
+    let headers = new Headers();
+	headers.set('content-type', 'application/json');
+    fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: headers
+        })
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+        });
+}
+
+
+
+function getData() {
+    let url = 'https://api.sheety.co/b5451f2c22c91b7055c9c9714d070461/test1Sheets/sheet1';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            console.log(json.sheet1[0]);
+        });
+}
