e9c335f449fad06eac5b183af2e6ba93ac53641c
Sujit <sjoshi1@saic.edu>
2021-07-01 23:30:28 +0530

    junior stuff

--- codingwork/template-signup-form-master/test1_script.js
+++ codingwork/template-signup-form-master/test1_script.js
@@ -39,20 +39,20 @@ function postData(lat,long) {
             longValue: long
         }
     };
-    let headers = new Headers();
-	headers.set('content-type', 'application/json');
-    fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: headers
-        })
-        .then((response) => response.json())
-        .then(json => {
-            console.log(json);
-        });
+    let website = 'https://maps.google.com/maps?q='+lat+',%20'+long; //https://maps.google.com/maps?q=1%20Grafton%20Street,%20Dublin,%20Ireland
+    document.getElementById('map').src=website;
+    // let headers = new Headers();
+	// headers.set('content-type', 'application/json');
+    // fetch(url, {
+            // method: 'POST',
+            // body: JSON.stringify(body),
+            // headers: headers
+        // })
+        // .then((response) => response.json())
+        // .then(json => {
+            // console.log(json);
+        // });
 }
-
-
 
 function getData() {
     let url = 'https://api.sheety.co/b5451f2c22c91b7055c9c9714d070461/test1Sheets/sheet1';
