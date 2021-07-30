5f52dfd752f8e0696d8723da776b4d07f10fdeb8
Sujit <sjoshi1@saic.edu>
2021-07-31 00:27:25 +0530

    pos

---
+++ codingwork/humanqind/parents-orientation/main.js
@@ -0,0 +1,49 @@
+let dateTime;
+let day;
+let hour;
+let minutes;
+let session=['https://youtu.be/Fe6d9x_LcOA','https://youtu.be/ssLyfHbxEa8','sesh3','sesh4','sesh5'];
+
+
+window.addEventListener('DOMContentLoaded', getHour);
+
+function getHour(){
+    const today = new Date();
+    day = today.getDate();
+    hour = today.getHours();
+    minutes = today.getMinutes();
+
+    console.log(day+' '+hour+' '+minutes);
+    // const date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    // const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    if (day===31 && hour<=13 && hour>=11) {
+        document.getElementById('h2').innerHTML='Please click on the link below to go to the session';
+        document.getElementById('banner').style.display='none';
+
+    }
+
+    if (day===31 && hour>=17 && hour<=19) {
+        // document.getElementById('h').innerHTML=session[1];
+        document.getElementById('h2').innerHTML='Please click on the link below to go to the session';
+        document.getElementById('banner').style.display='none';
+    }
+
+    else{
+        document.getElementById('h2').innerHTML='Session stream has not started yet. Please check back as per the scheduled time';
+        document.getElementById('button').style.display='none';
+        document.getElementById('banner').style.display='block';
+    }
+}
+
+function openSession() {
+    if (day===31 && hour>=11 && hour<=13) {
+        window.open(session[2]);
+    }
+
+    if (day===31 && hour>=17 && hour<=19) {
+        // document.getElementById('h').innerHTML=session[1];
+        window.open(session[1]);
+    }
+}
+
