75a8c93a18c54d2c69bd98de4be5dd103387a1a6
Sujit <sjoshi1@saic.edu>
2021-07-31 17:51:24 +0530

    forms

---
+++ codingwork/humanqind/reopeningMindsets/main.js
@@ -0,0 +1,8 @@
+
+function preSchoolto8() {
+    window.open('https://forms.gle/QC1wT3oapdNr9jcQ9');
+}
+
+function class9to12() {
+    window.open('https://forms.gle/qknYqME5Gt4nogfE6');
+}
---
+++ codingwork/humanqind/travelSurvey/main.js
@@ -0,0 +1,12 @@
+
+function prePrimary() {
+    window.open('https://forms.gle/Vo2EcchBnA3QqDB59');
+}
+
+function class1to8() {
+    window.open('https://forms.gle/VX1d2ycD6HYfNhke7');
+}
+
+function class9to12() {
+    window.open('https://forms.gle/weKdJToteWorRcor7');
+}
