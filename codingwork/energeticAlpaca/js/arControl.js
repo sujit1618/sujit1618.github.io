12c7fdc39ea628c60ae57605a608bdbfaa5e7521
Sujit <sjoshi1@saic.edu>
2022-03-27 14:06:00 +0530

    c

--- codingwork/energeticAlpaca/js/arControl.js
+++ codingwork/energeticAlpaca/js/arControl.js
@@ -12,19 +12,29 @@ function logger(x) {
     window.location='./videos/rhine.mp4';
   }
   function openSurf(){
-    window.location='https://www.youtube.com/watch?v=Uqmc0891L2U';
+    window.location='https://www.youtube.com/watch?v=Uqmc0891L2U?fs=1';
   }
   function openRoyalenf(){
     window.location='https://www.royalenfield.com/in/en/motorcycles/continental-gt/';
   }
+  function openCanon(){
+    window.location='https://www.youtube.com/watch?v=Fe45iRMSwRc';
+  }
+  function openLakehotel(){
+    window.location='http://museumsrajasthan.gov.in/VirtualTour/amber/index.html';
+  }
 
   function inHover() {
     let cursorEnt = document.getElementById('cursorEnt');
-    cursorEnt.setAttribute("material","color: teal; shader: flat; opacity: 0.6;");
+    let circleEnt = document.getElementById('selectionCirlce');
+    // cursorEnt.setAttribute("material","color: teal; shader: standard; opacity:1;");
+    circleEnt.setAttribute("material","side: double; color:teal; opacity: 0.4");
     //   console.log(x);
   }
 
   function outHover(){
     let cursorEnt = document.getElementById('cursorEnt');
-    cursorEnt.setAttribute("material","color: white; shader: flat; opacity: 0.3");
+    let circleEnt = document.getElementById('selectionCirlce');
+    // cursorEnt.setAttribute("material","color: white; shader: flat; opacity: 0.3");
+    circleEnt.setAttribute("material","side: double; color:teal; opacity: 0");
   }
