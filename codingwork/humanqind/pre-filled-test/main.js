ccafeb14b47ab9ee05d37432ea454168fc66908e
Sujit <sjoshi1@saic.edu>
2022-11-19 15:50:44 +0530

    c

--- codingwork/humanqind/pre-filled-test/main.js
+++ codingwork/humanqind/pre-filled-test/main.js
@@ -20,8 +20,16 @@ function loadScreen2() {
         document.getElementById('screen2').style.display = "block";
         console.log(studentName, admissionNumber);
     }
-
 }
+
+    function loadScreen1() {
+        document.getElementById('screen1').style.display = "block";
+        document.getElementById('screen2').style.display = "none";
+        document.getElementById('studentName').value = studentName;
+        document.getElementById('admissionNumber').value = admissionNumber;
+
+    }
+
 // function loadScreen2(){
 // studentName = document.getElementById('studentName').value;
 // admissionNumber = document.getElementById('admissionNumber').value;  
@@ -30,6 +38,7 @@ function loadScreen2() {
 // }
 
 function loadScreen3() {
+    
     document.getElementById('screen2').style.display = "none";
     document.getElementById('screen3').style.display = "block";
 }
@@ -66,64 +75,15 @@ function homeBucket(lat, long) {
     console.log(homeLat, homeLong);
 }
 
-function loadScreen5() {
-    document.getElementById('screen4').style.display = "none";
-    document.getElementById('screen5').style.display = "block";
+function loadScreen4() {
+    document.getElementById('screen9').style.display = "none";
+    document.getElementById('screen4').style.display = "block";
     console.log(homeLat, homeLong);
 }
 
-function vanUsagePage() {
-    let vanUsage = document.querySelector('input[name="van-travel"]:checked').value;
-    if (vanUsage === 'yes') {
-        document.getElementById('screen5').style.display = "none";
-        document.getElementById('screen6').style.display = "block";
-    } else if (vanUsage === 'no') {
-        document.getElementById('screen5').style.display = "none";
-        document.getElementById('screen9').style.display = "block";
-    }
-}
-
-function loadScreen7() {
-    document.getElementById('screen6').style.display = "none";
-    document.getElementById('screen7').style.display = "block";
-}
-
-function vanLocation() {
-
-    function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        vanBucket(latitude, longitude);
-    }
-
-    function error() {
-        status.textContent = 'Unable to retrieve your location';
-    }
-
-    if (!navigator.geolocation) {
-        status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-        status.textContent = 'Locating…';
-        navigator.geolocation.getCurrentPosition(success, error);
-    }
-    document.getElementById('but-7').innerHTML = 'Please click again in 5 seconds';
-    if (vanLat > 1) {
-        document.getElementById('screen7').style.display = "none";
-        document.getElementById('screen8').style.display = "block";
-    }
-
-}
-
-function vanBucket(lat, long) {
-    vanLat = lat;
-    vanLong = long;
-    console.log(vanLat, vanLong);
-}
-
 function loadScreen9() {
-    document.getElementById('screen8').style.display = "none";
+    document.getElementById('screen4').style.display = "none";
     document.getElementById('screen9').style.display = "block";
-    console.log(vanLat, vanLong);
 }
 
 function loadScreen10() {
@@ -137,8 +97,6 @@ function loadScreen10() {
             admissionNumber,
             homeLat,
             homeLong,
-            vanLat,
-            vanLong,
         }
     };
     console.log(body);
@@ -160,11 +118,11 @@ function prefilledform() {
     let url = 'https://docs.google.com/forms/d/e/1FAIpQLSekRoDUtU8-O3FepafQW9rOWtK-LUJ0x7Lak_hhCJumCJkFsg/viewform?usp=pp_url';
     let radio = '&entry.1591633300='; //radio
     let feedback = '&entry.326955045='; //feedback
-    let suggestions = '&entry.1696159737=';//suggestions
+    let suggestions = '&entry.1696159737='; //suggestions
     let name = '&entry.485428648='; //name
     let email = '&entry.879531967='; //email
 
-    let prefilledUrl = url + radio + 'Bug+Reportsss'+ feedback + '' + suggestions + '' + name + studentName + email + admissionNumber;
+    let prefilledUrl = url + radio + 'Bug+Reports' + feedback + '' + suggestions + '' + name + studentName + email + admissionNumber;
     window.open(prefilledUrl);
 }
 
